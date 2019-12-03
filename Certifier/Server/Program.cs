c034e9a1081ef486764341ffd962b897bd6c19f6
ironpython2001 <ironpython2001@gmail.com>
2019-12-03 18:25:00 +0530

    updated code

--- Certifier/Server/Program.cs
+++ Certifier/Server/Program.cs
@@ -1,21 +1,3 @@
-#region Copyright notice and license
-
-// Copyright 2019 The gRPC Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-#endregion
-
 using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
@@ -38,7 +20,7 @@ namespace Server
                 {
                     webBuilder.ConfigureKestrel(kestrelOptions =>
                     {
-                         kestrelOptions.ConfigureEndpointDefaults(lo => lo.Protocols = HttpProtocols.Http2);
+                        kestrelOptions.ConfigureEndpointDefaults(lo => lo.Protocols = HttpProtocols.Http2);
                         kestrelOptions.ConfigureHttpsDefaults(httpsOptions =>
                         {
                             httpsOptions.ClientCertificateMode = ClientCertificateMode.AllowCertificate;
